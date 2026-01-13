90687380fd8d4e1837412eb9702ed23c273a377d
Surya <suryareddynallimilli@gmail.com>
2026-01-13 15:40:41 +0530

    manual serialization

--- api/views.py
+++ api/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from students.models import *
 # Create your views here.
 def studentsView(request):
-    students={
-        'id': 1,
-        'name':'USHA',
-        'branch':'AIM'
-    }
-    return JsonResponse(students)
- 
+    students= Students.objects.all()
+    students_list=list(students.values()) 
+    return JsonResponse(students_list,safe=False)
+  
--- students/models.py
+++ students/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    student_id=models.CharField(max_length=10)
+    name=models.CharField(max_length=100)
+    branch=models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name_plural = 'Students'
+    
+    def __str__(self):
+        return self.name
